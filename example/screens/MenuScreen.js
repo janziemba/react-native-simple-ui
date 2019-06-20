715520626c80cd8e5dde7a128da555c73df08e95
Jan Ziemba <jan.ziemba@gmail.com>
2019-06-20 17:48:38 +0200

    docs: use ListItems instead of Buttons in menu

--- example/screens/MenuScreen.js
+++ example/screens/MenuScreen.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React, { PureComponent } from 'react';
-import { Button, Container, Spacer } from 'react-native-simple-ui';
+import { Container, ListItem, Text } from 'react-native-simple-ui';
 
 import ScreenContainer from '../components/ScreenContainer';
 
@@ -10,80 +10,40 @@ const propTypes = {
     }).isRequired,
 };
 
+const SCREENS = [
+    'Avatar', 'Button', 'Card', 'Container', 'Grid', 'Icon', 'LinearGradient',
+    'ListItem', 'Modal', 'Rating', 'TextLink', 'Text', 'Toolbar',
+];
+
 class MenuScreen extends PureComponent {
     static navigationOptions = {
         title: 'Menu',
     };
-    render() {
+    constructor(props) {
+        super(props);
+
+        this.renderListItem = this.renderListItem.bind(this);
+    }
+    renderListItem(screen) {
         const { navigation: { navigate } } = this.props;
 
         return (
+            <ListItem
+                key={screen}
+                centerElement={(
+                    <Text>
+                        {screen}
+                    </Text>
+                )}
+                onPress={() => navigate(screen)}
+            />
+        );
+    }
+    render() {
+        return (
             <ScreenContainer>
-                <Container paddingSize="medium">
-                    <Button
-                        onPress={() => navigate('Avatar')}
-                        text="Avatar"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Button')}
-                        text="Button"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Card')}
-                        text="Card"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Container')}
-                        text="Container"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Grid')}
-                        text="Grid"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Icon')}
-                        text="Icon"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('LinearGradient')}
-                        text="LinearGradient"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('ListItem')}
-                        text="ListItem"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Modal')}
-                        text="Modal"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Rating')}
-                        text="Rating"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Text')}
-                        text="Text"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('TextLink')}
-                        text="TextLink"
-                    />
-                    <Spacer />
-                    <Button
-                        onPress={() => navigate('Toolbar')}
-                        text="Toolbar"
-                    />
+                <Container>
+                    {SCREENS.map(screen => this.renderListItem(screen))}
                 </Container>
             </ScreenContainer>
         );
